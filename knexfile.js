9f6f339e2eaa9d17ac4b5014587ea73d154e0277
amodinho <u14284783@tuks.co.za>
2019-07-12 15:19:54 +0200

    added pooling

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,7 @@ module.exports = {
             user: process.env.db_user,
             password: process.env.db_password,
             database: process.env.db_name,
-        }
+        },
+        pool: {min:0, max: 10}
     }
 }
