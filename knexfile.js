44b0f7b975ecf24434c7c54bed68d3decad33bcc
amodinho <u14284783@tuks.co.za>
2019-07-15 16:00:25 +0200

    changed the host conenct

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
    
         client: 'mysql',
         connection: {
-            host: process.env.db_url,
+            host: process.env.db_host,
             user: process.env.db_user,
             password: process.env.db_password,
             database: process.env.db_name,
