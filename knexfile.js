73d2d6c3a2f41a82bc1f6844a9193dab7357c71c
amodinho <u14284783@tuks.co.za>
2019-07-16 14:28:24 +0200

    removing nonsese

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
    
         client: 'mysql',
         connection: {
-            host: process.env.db_host,
+            host: process.env.db_url,
             user: process.env.db_user,
             password: process.env.db_password,
             database: process.env.db_name,
