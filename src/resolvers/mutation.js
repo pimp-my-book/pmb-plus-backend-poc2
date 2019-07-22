6af4a9ccfe75dc7a08ead99b97ee233c61b20d9a
amodinho <u14284783@tuks.co.za>
2019-07-22 20:14:23 +0200

    it is working

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -25,21 +25,30 @@ var clientLocal = Client({
     }
 })
 console.log(args.title)
-console.log(client.config)
+console.log(clientLocal.config)
 
 
 try{
 
-    await client.connect()
+    await clientLocal.connect()
 
+    await clientLocal.query(`
+    CREATE TABLE IF NOT EXISTS books
+    (
+        id MEDIUMINT UNSIGNED not null AUTO_INCREMENT,
+        title varchar(100) not null,
+        author varchar(100) not null,
+        PRIMARY KEY (id)
+    );
+    `)
 
     
-    let con = client.getClient()
+    let con = clientLocal.getClient()
     console.log(con)
     console.log(args.title,args.author)
-    let book = await client.query('INSERT INTO books (title,author) VALUES(?,?)', [args.title ,args.author ]);
+    let book = await clientLocal.query('INSERT INTO books (title,author) VALUES(?,?)', [args.title ,args.author ]);
     console.log(book)
-    await client.end()
+    await clientLocal.end()
      
     return {
         title: args.title,
