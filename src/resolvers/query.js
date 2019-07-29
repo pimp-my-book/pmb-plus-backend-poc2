6d97a1f91807cda142bea5fa7a3a5294471b9a51
amodinho <u14284783@tuks.co.za>
2019-07-29 14:31:09 +0200

    added query to shell

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,3 +1,11 @@
+import db from '../../libs/db'
+
+
 export const hello = (args, context) => {
     return "Your GraphQL API is now LIVE!🎈 "
+}
+
+
+export const getAllBooks = async (args, context) => {
+
 }
