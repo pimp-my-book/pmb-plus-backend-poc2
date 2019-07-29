af8624e07d57d8389ae521981276487b24d7fb7a
amodinho <u14284783@tuks.co.za>
2019-07-29 15:19:18 +0200

    referenced it in index

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,10 +1,11 @@
-import {hello} from "./query";
+import {hello, getAllBooks} from "./query";
 import {addBook, addVendor} from "./mutation";
 import {db} from '../../libs/db'
 export const resolvers = {
     Query: {
         hello: (root, args, context) => hello(args, context),
-        connection: () => `${db}`
+        connection: () => `${db}`,
+        getAllBooks: (root, args, contenxt) => getAllBooks(args,contenxt)
     },
     Mutation : {
         addBook: (root, args, context) => addBook(args, context),
