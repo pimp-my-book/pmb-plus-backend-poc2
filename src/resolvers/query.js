3c20a42dc879ec949afba275b5f012759d6cc902
amodinho <u14284783@tuks.co.za>
2019-07-30 17:36:37 +0200

    returned the single book

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -39,13 +39,25 @@ export const getOneBook = async (args, contenxt) =>{
 
     try {
 
-        const singleBook = db.query(`SELECT book.bookId, book.bookAuthor, book.bookISBN,  book.bookGrade, book.price , book.bookEdition , book.bookTitle, book.bookImage, vendor.vendorName 
+        const singleBook = await db.query(`SELECT book.bookId, book.bookAuthor, book.bookISBN,  book.bookGrade, book.price , book.bookEdition , book.bookTitle, book.bookImage, vendor.vendorName 
         FROM book
         inner JOIN vendor on book.vendorId = vendor.vendorId
-        WHERE bookId = ?`, [args.ID])
+        WHERE book.bookId = ?`, [args.ID])
 
-        return singleBook
-        
+        await db.end()
+        console.log(singleBook)
+        return {
+            title: singleBook[0].bookTitle,
+            vendor: singleBook[0].vendorName,
+            ID: singleBook[0].bookId,
+            price: singleBook[0].price,
+            image: singleBook[0].bookImage,
+            author: singleBook[0].bookAuthor,
+            ISBN: singleBook[0].bookISBN,
+            edition: singleBook[0].bookEdition,
+            grade: singleBook[0].bookGrade
+        }
+
     } catch (e){
         return e
     }
